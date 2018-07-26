e83a4bc0064ccb40485289c6d006c72ecc62fcb3
Lupitayoh <lupitayoh-5@hotmail.com>
2018-07-25 23:26:48 -0500

    Customer ws

--- src/main/java/com/yoh/WsPortfolio/models/Customer.java
+++ src/main/java/com/yoh/WsPortfolio/models/Customer.java
@@ -1,7 +1,6 @@
 package com.yoh.WsPortfolio.models;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**@author Lupita Llama
@@ -20,9 +19,6 @@ public class Customer {
 
     @Column(name = "email")
     private String email;
-
-    @Column(name = "password")
-    private String password;
 
     @Column(name = "phone")
     private String phone;
@@ -61,14 +57,6 @@ public class Customer {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getPhone() {
@@ -119,13 +107,16 @@ public class Customer {
         this.is_activated = is_activated;
     }
 
+    public Customer(){
+
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
                 ", phone='" + phone + '\'' +
                 ", cellphone='" + cellphone + '\'' +
                 ", is_activated=" + is_activated +
--- src/main/java/com/yoh/WsPortfolio/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/yoh/WsPortfolio/services/impl/CustomerServiceImpl.java
@@ -3,8 +3,13 @@ package com.yoh.WsPortfolio.services.impl;
 import com.yoh.WsPortfolio.models.Customer;
 import com.yoh.WsPortfolio.repositories.CustomerRepository;
 import com.yoh.WsPortfolio.services.CustomerService;
+import javassist.NotFoundException;
+import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
@@ -14,6 +19,13 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public Customer getCustomer(Long id) {
-        return customerRepository.getOne(id);
+        Optional<Customer> fooOptional = customerRepository.findById(id);
+        if (fooOptional.isPresent()){
+            Customer foo = fooOptional.get();
+            return  foo;
+        }
+        else{
+            return null;
+        }
     }
 }
